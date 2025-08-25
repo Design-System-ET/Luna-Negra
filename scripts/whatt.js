d68196883ebe66f627f12b749c2013af55a7ed5a
Design-System-ET <darkman_anubis_iara@hotmail.com>
2025-08-25 00:12:34 -0300

    creamos la pagina tarot

---
+++ scripts/whatt.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("enviar_tarot").addEventListener("click", function() {
+        window.open(
+            "https://api.whatsapp.com/send?phone=59898582316&text=Buenas%2C%20me%20gustar%C3%ADa%20hacer%20una%20consulta...", 
+            "_blank"
+        );
+    });
+});
